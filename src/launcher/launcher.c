7b8fa95aa3c8ff263e3d4b50c36444bc0b41ad8a
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2023-05-14 21:57:03 +0200

    Initialize branch

--- src/launcher/launcher.c
+++ src/launcher/launcher.c
@@ -3,43 +3,58 @@
 #include "sync.c"
 #include "util.c"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-	int cpuInfo[4] = {0};
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
+{
+	// Gather CPU info
+	int cpuInfo[4] = { 0 };
 	__cpuid(cpuInfo, 0x80000007);
 
+	// Check if the CPU supports the "Invariant TSC" feature
 	AssertMessageBox(cpuInfo[3] & (1 << 8),
 		L"Your processor does not support the \"Invariant TSC\" feature.\r\n\r\n"
 		L"VanillaFixes can only work reliably on modern (2007+) Intel and AMD processors."
 	);
 
 	LPWSTR pWowDirectory = HeapAlloc(GetProcessHeap(), 0, MAX_PATH * sizeof(WCHAR));
+	if (pWowDirectory == NULL)
+	{
+		MessageBox(NULL, L"Failed to allocate memory.", L"VanillaFixes", MB_OK | MB_ICONERROR);
+		return 1;
+	}
+
 	GetModuleFileName(NULL, pWowDirectory, MAX_PATH);
 
-	/* Don't attempt to resize the buffer - 1.12 won't be able to run an executable with a long UNC path anyway */
+	// Check if the buffer was large enough
 	AssertMessageBox(GetLastError() != ERROR_INSUFFICIENT_BUFFER, L"GetLastError() == ERROR_INSUFFICIENT_BUFFER");
 
+	// Remove the file name from the directory path
 	PathRemoveFileSpec(pWowDirectory);
 
+	// Get paths for the WoW exe, patcher dll, and nampower dll
 	LPWSTR pWowExePath = UtilGetPath(pWowDirectory, L"WoW.exe");
 	LPWSTR pPatcherPath = UtilGetPath(pWowDirectory, L"VfPatcher.dll");
 	LPWSTR pNamPowerPath = UtilGetPath(pWowDirectory, L"nampower.dll");
 
+	// Check if the necessary files exist
 	AssertMessageBox(GetFileAttributes(pWowExePath) != INVALID_FILE_ATTRIBUTES,
 		L"WoW.exe not found. Extract VanillaFixes into the same directory as the game.");
 	AssertMessageBox(GetFileAttributes(pPatcherPath) != INVALID_FILE_ATTRIBUTES,
 		L"VfPatcher.dll not found. Extract all files into the game directory.");
 
-	STARTUPINFO startupInfo = {0};
-	PROCESS_INFORMATION processInfo = {0};
+	STARTUPINFO startupInfo = { 0 };
+	PROCESS_INFORMATION processInfo = { 0 };
 
 	startupInfo.cb = sizeof(startupInfo);
 
+	// Prepare command line arguments for WoW
 	LPWSTR pWowCmdLine = UtilGetWowCmdLine(pWowExePath);
 	DWORD flags = CREATE_SUSPENDED | CREATE_UNICODE_ENVIRONMENT;
 
-	if(!CreateProcess(NULL, pWowCmdLine, NULL, NULL, FALSE, flags, NULL, NULL, &startupInfo, &processInfo)) {
+	// Create the WoW process
+	if (!CreateProcess(NULL, pWowCmdLine, NULL, NULL, FALSE, flags, NULL, NULL, &startupInfo, &processInfo))
+	{
 		LPWSTR errorMessage = UtilGetLastError();
-		WCHAR scratchBuffer[512] = {0};
+		WCHAR scratchBuffer[512] = { 0 };
 
 		swprintf(scratchBuffer,
 			512,
@@ -53,26 +68,31 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
 	int injectError = RemoteLoadLibrary(pPatcherPath, processInfo.hProcess);
 
-	/* Also inject Nampower if present in the game directory */
-	if(GetFileAttributes(pNamPowerPath) != INVALID_FILE_ATTRIBUTES) {
+	// Also inject Nampower if present in the game directory
+	if (GetFileAttributes(pNamPowerPath) != INVALID_FILE_ATTRIBUTES)
+	{
 		injectError = injectError || RemoteLoadLibrary(pNamPowerPath, processInfo.hProcess);
 
 		g_sharedData.initNamPower = TRUE;
 		RemoteSyncData(processInfo.hProcess);
 	}
 
-	if(injectError) {
+	if (injectError)
+	{
 		TerminateProcess(processInfo.hProcess, 0);
 		return injectError;
 	}
 
-	/* Remote data will be freed by VfPatcher.dll */
+	// Remote data will be freed by VfPatcher.dll
 	g_pRemoteData = NULL;
 
+	// Resume the WoW process
 	ResumeThread(processInfo.hThread);
 
+	// Close handles to the process and the main thread after the process has been resumed
 	CloseHandle(processInfo.hProcess);
 	CloseHandle(processInfo.hThread);
 
+	// End the process normally
 	return 0;
 }
--- src/launcher/sync.c
+++ src/launcher/sync.c
@@ -1,8 +1,15 @@
-static VF_SHARED_DATA g_sharedData = {0};
+
+// Global shared data variable and remote data pointer
+static VF_SHARED_DATA g_sharedData = { 0 };
 static PVF_SHARED_DATA g_pRemoteData = NULL;
 
-BOOL RemoteSyncData(HANDLE hTargetProcess) {
-	if(!g_pRemoteData) {
+// Function to synchronize shared data with the target process
+BOOL RemoteSyncData(HANDLE hTargetProcess)
+{
+	// If the remote data pointer has not been allocated yet
+	if (!g_pRemoteData)
+	{
+		// Allocate memory in the target process for the shared data structure
 		g_pRemoteData = VirtualAllocEx(
 			hTargetProcess,
 			NULL,
@@ -11,27 +18,55 @@ BOOL RemoteSyncData(HANDLE hTargetProcess) {
 			PAGE_READWRITE
 		);
 
-		WriteProcessMemory(hTargetProcess, g_pSharedData, &g_pRemoteData, sizeof(PVF_SHARED_DATA*), NULL);
+		// Check if VirtualAllocEx was successful
+		if (g_pRemoteData == NULL)
+		{
+			return FALSE;
+		}
+
+		// Write the address of the shared data structure to the target process's memory
+		if (!WriteProcessMemory(hTargetProcess, g_pSharedData, &g_pRemoteData, sizeof(PVF_SHARED_DATA*), NULL))
+		{
+			return FALSE;
+		}
 	}
 
-	return WriteProcessMemory(hTargetProcess, g_pRemoteData, &g_sharedData, sizeof(VF_SHARED_DATA), NULL);
+	// Write the shared data to the target process's memory
+	if (!WriteProcessMemory(hTargetProcess, g_pRemoteData, &g_sharedData, sizeof(VF_SHARED_DATA), NULL))
+	{
+		return FALSE;
+	}
+
+	return TRUE;
 }
 
-int RemoteLoadLibrary(LPWSTR pDllPath, HANDLE hTargetProcess) {
+// Function to load a DLL into the target process
+int RemoteLoadLibrary(LPWSTR pDllPath, HANDLE hTargetProcess)
+{
+	// Copy the DLL path to the shared data structure
 	StrCpy(g_sharedData.pDllPath, pDllPath);
+
+	// Assert that the shared data was successfully written to the target process's memory
 	AssertMessageBox(RemoteSyncData(hTargetProcess), L"Failed to write process memory (pDllPath)");
 
+	// Create a new thread in the target process that will load the DLL
 	HANDLE hThread =
 		CreateRemoteThread(hTargetProcess, NULL, 0, (LPTHREAD_START_ROUTINE)&LoadLibraryW, &g_pRemoteData->pDllPath, 0, NULL);
+
+	// Assert that the thread was successfully created
 	AssertMessageBox(hThread, L"Failed to create remote thread");
 
+	// Wait for the created thread to terminate
 	WaitForSingleObject(hThread, 10000);
 
 	DWORD threadResult = 0;
+	// Retrieve the termination status of the thread
 	GetExitCodeThread(hThread, &threadResult);
 
+	// Close the thread handle
 	CloseHandle(hThread);
 
+	// Assert that the thread terminated successfully and the DLL was loaded
 	AssertMessageBox(threadResult && threadResult != STILL_ACTIVE,
 		L"DllMain failed after injection.\r\n\r\n"
 		L"Make sure you have a compatible game client."
